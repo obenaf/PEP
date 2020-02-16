7c6307f768621e244194d35d11580a0a75491a05
LukeRyssel <51679209+LukeRyssel@users.noreply.github.com>
2020-02-16 14:07:05 -0800

    Updates

---
+++ src/Assets/Luke Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    float range;
+    float height;
+    string inputY;
+    string inputX;
+    
+    void Start()
+    {
+        height = transform.localScale.y;
+        range = transform.localScale.x;
+        speed = 5f;
+        inputY = "Vertical";
+        inputX = "Horizontal";
+      
+    }
+
+    
+    void Update()
+    {
+        float moveY = Input.GetAxis(inputY) * Time.deltaTime * speed;
+        transform.Translate(moveY * Vector2.up);
+        float moveX = Input.GetAxis(inputX) * Time.deltaTime * speed;
+        transform.Translate(moveX * Vector2.right);
+    }
+}
