80039532d286274a1a53c4d645754e9a45d35f15
LukeRyssel <51679209+LukeRyssel@users.noreply.github.com>
2020-02-17 13:40:17 -0800

    Updates

--- src/Assets/Luke Assets/Player.cs
+++ src/Assets/Luke Assets/Player.cs
@@ -5,29 +5,70 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    float speed;
+    public float speed;
 
-    float range;
-    float height;
-    string inputY;
-    string inputX;
-    
+    private float moveX, moveY, changeTurn;
+    private Rigidbody2D myRigidbody;
+    private Vector2 velocity;
+    public bool PlayerTurn;
     void Start()
     {
-        height = transform.localScale.y;
-        range = transform.localScale.x;
-        speed = 5f;
-        inputY = "Vertical";
-        inputX = "Horizontal";
-      
+        myRigidbody = GetComponent<Rigidbody2D>();
+        PlayerTurn = true;
+
     }
 
-    
+
     void Update()
     {
-        float moveY = Input.GetAxis(inputY) * Time.deltaTime * speed;
-        transform.Translate(moveY * Vector2.up);
-        float moveX = Input.GetAxis(inputX) * Time.deltaTime * speed;
-        transform.Translate(moveX * Vector2.right);
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetAxisRaw("Vertical");
+        changeTurn = Input.GetAxisRaw("ChangeTurn");
+
+        if (PlayerTurn == true)
+        {
+            if (moveX != 0)
+            {
+                movePlayer(moveX, 0);
+            }
+            if (moveY != 0)
+            {
+                movePlayer(0, moveY);
+            }
+        }
+        if (changeTurn == 1)
+        {
+            PlayerTurn = true;
+        }
+        
+
     }
+    //move Player = Direction needs to be a 
+    void movePlayer(float directionX, float directionY)
+    {
+        
+        if (directionX == 1)
+        {
+            velocity = new Vector2(.5f, -.5f);
+            myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+        }
+        if (directionX == -1)
+        {
+            velocity = new Vector2(-.5f, .5f);
+            myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+        }
+        if (directionY == 1)
+        {
+            velocity = new Vector2(.5f, .5f);
+            myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+        }
+        if (directionY == -1)
+        {
+            velocity = new Vector2(-.5f, -.5f);
+            myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+        }
+        //PlayerTurn = false;
+    }
+    
 }
+ //* Time.fixedDeltaTime
