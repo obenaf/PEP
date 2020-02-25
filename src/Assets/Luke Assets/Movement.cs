d16340131f6d913ffb9b8647a989b6e16a32840d
LukeRyssel <51679209+LukeRyssel@users.noreply.github.com>
2020-02-25 10:00:55 -0800

    Changes

--- src/Assets/Luke Assets/Movement.cs
+++ src/Assets/Luke Assets/Movement.cs
@@ -13,13 +13,14 @@ public class Movement : MonoBehaviour
     private Rigidbody2D myRigidbody;
     private Vector2 velocity;
     private float oldpositionX, oldpositionY;
-    
+    private Animator Mainanim;//animation variable for the name of the animator
     
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         PlayerTurn = true;
         movement = 3f;
+        Mainanim = GetComponent<Animator>();//set the variable to the name of the animator controller
     }
 
 
@@ -47,6 +48,9 @@ public class Movement : MonoBehaviour
 
         oldpositionX = myRigidbody.position.x;
         oldpositionY = myRigidbody.position.y;
+
+        Mainanim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));//sets the MoveX for animation
+        Mainanim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));//sets the MoveX for animation
 
     }
     
