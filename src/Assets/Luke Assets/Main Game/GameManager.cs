294c4d78a4e281ab8bd8aa9ab121e6ced76b673c
LukeRyssel <51679209+LukeRyssel@users.noreply.github.com>
2020-02-25 16:34:20 -0800

    Changes

---
+++ src/Assets/Luke Assets/Main Game/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool combatState;//True is in combat, false is out of combat
+    void Start()
+    {
+        combatState = false;
+    }
+
+    void Update()
+    {
+        
+    }
+    bool inCombat(){// gives state of combat to other scripts
+        if (combatState == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+}
+}
+
+
--- src/Assets/Luke Assets/Movement.cs
+++ src/Assets/Luke Assets/Movement.cs
@@ -13,14 +13,12 @@ public class Movement : MonoBehaviour
     private Rigidbody2D myRigidbody;
     private Vector2 velocity;
     private float oldpositionX, oldpositionY;
-    //private Animator Mainanim;//animation variable for the name of the animator
     
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         PlayerTurn = true;
         movement = 3f;
-       // Mainanim = GetComponent<Animator>();//set the variable to the name of the animator controller
     }
 
 
@@ -49,11 +47,7 @@ public class Movement : MonoBehaviour
         oldpositionX = myRigidbody.position.x;
         oldpositionY = myRigidbody.position.y;
 
-       // Mainanim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));//sets the MoveX for animation
-       // Mainanim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));//sets the MoveX for animation
-
     }
-    
     void movePlayer(float directionX, float directionY)
     {
             velocity = new Vector2(directionX, directionY);
