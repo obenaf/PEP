e5814613df972c55073430131cbe3d65d71a22e4
Doug Barnes <Doug.barnes27@gmail.com>
2020-03-03 19:00:00 -0800

    Added main menu functionality
    you can now play game, view options, and exit from main menu

---
+++ src/Assets/Scenes/MainMenu/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Hops over to next scene, not specific.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
