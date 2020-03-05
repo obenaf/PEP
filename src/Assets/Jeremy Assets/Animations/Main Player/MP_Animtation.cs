37e024044eaa77b3b9eb7fcdbe837a56035a6175
1Robbin <60487612+1Robbin@users.noreply.github.com>
2020-03-05 13:30:20 -0800

    inventory code

--- src/Assets/Jeremy Assets/Animations/Main Player/MP_Animtation.cs
+++ src/Assets/Jeremy Assets/Animations/Main Player/MP_Animtation.cs
@@ -5,28 +5,20 @@ using UnityEngine;
 public class MP_Animtation : MonoBehaviour
 {
     private Animator Mainanim;//animation variable for the name of the animator
-    public GameObject levelManager;//Call methods from the Level0Manager.cs
-    public Level0Manager levelManagerScripts;
-
-   
     // Start is called before the first frame update
     void Start()
     {
         Mainanim = GetComponent<Animator>();//set the variable to the name of the animator controller
-        levelManager = GameObject.FindGameObjectWithTag("level0Manager");
-        levelManagerScripts = levelManager.GetComponent<Level0Manager>();
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (levelManagerScripts.turnManager() == true)//Check if it is player turn
+    void FixedUpdate()
+    {//if (levelManagerScripts.turnManager() == true)
         {
             Mainanim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));//sets the MoveX for animation
             Mainanim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));//sets the MoveX for animation
             Mainanim.SetFloat("Attack", Input.GetAxisRaw("Fire1"));//sets the MoveX for animation
-
         }
     }
 }
