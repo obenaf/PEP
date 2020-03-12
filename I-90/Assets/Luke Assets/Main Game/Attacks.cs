4fddd4226cac395d4bc4fd83092fc88dacf77bf0
Richard <43195127+obenaf@users.noreply.github.com>
2020-03-12 15:49:07 -0700

    Initial recreation

---
+++ I-90/Assets/Luke Assets/Main Game/Attacks.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacks : MonoBehaviour
+{
+
+    public GameObject ArrowPrefab;
+    public GameObject playerMovement;
+    public PlayerMovement playerMovementScripts;
+
+    void Start()
+    {
+        playerMovement = GameObject.FindGameObjectWithTag("Player");
+        playerMovementScripts = playerMovement.GetComponent<PlayerMovement>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        
+    }
+
+    public int getMeleeDamage(int attack, int accuracy){//Returns a damage int based off the players attack
+        int damage;
+        int hitOrMiss = Random.Range(1, 100);
+        
+        if (hitOrMiss <= accuracy)
+        {
+            damage = Random.Range(1, attack);
+        }
+        else
+        {
+            damage = 0;
+        }
+        return damage;
+    }
+    public int getRangeDamage(int attack, int accuracy)
+    {
+        return 0;
+    }
+    public void spawnArrow()
+    {
+        GameObject a = Instantiate(ArrowPrefab) as GameObject;
+        a.transform.position = new Vector2(playerMovementScripts.getPositionX(), playerMovementScripts.getPositionY());
+    }
+
+}
---
+++ I-90/Assets/Luke Assets/Main Game/Level0Manager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level0Manager : MonoBehaviour
+{
+    GameObject player;
+    PlayerMovement playerScripts;
+
+    [SerializeField]
+    public bool playerTurn = true;//True is player's turn, false is enemy's turn
+
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScripts = player.GetComponent<PlayerMovement>();
+
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+    public void changeTurn(){
+        
+        playerTurn = !playerTurn;
+    }
+    public bool turnManager(){// gives state of combat to other scripts
+        if (playerTurn == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+
