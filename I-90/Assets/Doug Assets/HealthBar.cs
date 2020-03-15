4a7c52735ca15edc5731c1158ae451f72e70c559
Doug Barnes <Doug.barnes27@gmail.com>
2020-03-14 20:46:23 -0700

    Health Bar
    Added a health bar that can eventually be used by both enemies and players

---
+++ I-90/Assets/Doug Assets/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Learned how to make a health bar from: https://www.youtube.com/watch?v=Gtw7VyuMdDc
+public class HealthBar : MonoBehaviour
+{
+    private Transform bar;
+    
+    private void Start()
+    {
+        bar = transform.Find("Bar");
+    }
+
+    public void SetSize(float sizeNormalized)
+    {
+        bar.localScale = new Vector3(sizeNormalized, 1f);
+    }
+}
