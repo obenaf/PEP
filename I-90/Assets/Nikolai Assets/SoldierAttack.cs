7625951cffa4a8e874f8ea733b64c680067243a3
tion4994 <54964647+tion4994@users.noreply.github.com>
2020-03-21 15:06:26 -0700

    Added old attack script

---
+++ I-90/Assets/Nikolai Assets/SoldierAttack.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierAttack : Enemy {
+    public Player playerAttack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelManager = GameObject.FindGameObjectWithTag("level0Manager");
+        levelManagerScripts = levelManager.GetComponent<Level0Manager>();
+
+        //enemy = GameObject.FindGameObjectWithTag("Enemy");
+        SoldierMovement = GameObject.FindGameObjectWithTag("Enemy");
+
+        playerMovement = GameObject.FindGameObjectWithTag("Player");
+        playerMovementScripts = playerMovement.GetComponent<PlayerMovement>();
+        playerAttack = playerMovement.GetComponent<Player>();
+
+        //enemyMovementScripts = enemy.GetComponent<SoldierMovement>();
+        //enemyScripts = enemy.GetComponent<Enemy>();
+
+        attackOptions = GameObject.FindGameObjectWithTag("Attacks");
+        attackScripts = attackOptions.GetComponent<Attacks>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (levelManagerScripts.turnManager() == false)
+        {
+            if (attackPossible(range) == true)
+            {
+                int damage;
+                damage = attackScripts.getMeleeDamage(attack, accuracy);
+                playerAttack.damagePlayer(damage);
+                levelManagerScripts.changeTurn();
+            }
+        }
+    }
+
+    protected override bool attackPossible(float range)
+    {
+        return base.attackPossible(range);
+    }
+
+    /*public void attackPlayer() { 
+        if (levelManagerScripts.turnManager() == true)
+        {
+            if (attackPossible(range) == true)
+            {
+                int damage;
+                damage = attackScripts.getMeleeDamage(attack, accuracy);
+                playerAttack.damagePlayer(damage);
+                //levelManagerScripts.changeTurn();
+            }
+        }
+    }*/
+}
