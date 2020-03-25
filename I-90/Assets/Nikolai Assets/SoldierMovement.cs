47845baff7590161357c51bbee31ccdb5691e922
tion4994 <54964647+tion4994@users.noreply.github.com>
2020-03-25 16:51:04 -0700

    Added a patrol movement pattern

--- I-90/Assets/Nikolai Assets/SoldierMovement.cs
+++ I-90/Assets/Nikolai Assets/SoldierMovement.cs
@@ -6,9 +6,20 @@ public class SoldierMovement : Movement
 {
     public GameObject playerMovement;
     public PlayerMovement playerMovementScripts;
+    public float[,] waypoints;
     public int direction;
     public int moveCounter = 1;
+    private float speedModifier = 2;
     public bool changeDir = false;
+    public bool patrol = false;
+    public float waypoint1x;
+    public float waypoint1y;
+    public float waypoint2x;
+    public float waypoint2y;
+    public float waypoint3x;
+    public float waypoint3y;
+    public float MoveX, MoveY;
+    public int currentWaypoint = 1;
     void Start()
     {
         levelManager = GameObject.FindGameObjectWithTag("level0Manager");
@@ -27,12 +38,26 @@ public class SoldierMovement : Movement
 
         myRigidbody = GetComponent<Rigidbody2D>();
         movement = enemyScripts.getMovement();//changed
+
+        waypoints = new float[3, 2];
+        waypoints[0,0] = getPositionX();
+        waypoints[0,1] = getPositionY();
+        waypoints[1,0] = getPositionX()+1;
+        waypoints[1,1] = getPositionY();
+        waypoints[2,0] = getPositionX()+1;
+        waypoints[2,1] = getPositionY()+1;
+        waypoint1x = waypoints[0, 0];
+        waypoint1y = waypoints[0, 1];
+        waypoint2x = waypoints[1, 0];
+        waypoint2y = waypoints[1, 1];
+        waypoint3x = waypoints[2, 0];
+        waypoint3y = waypoints[2, 1];
     }
 
 
     void FixedUpdate()
     {
-        float MoveX, MoveY;
+        //float MoveX, MoveY;
 
         float playerPosX = playerMovementScripts.getPositionX();
         float playerPosY = playerMovementScripts.getPositionY();
@@ -43,7 +68,7 @@ public class SoldierMovement : Movement
         if (levelManagerScripts.turnManager() == false)
         {
             travelledTotal = getMovement(travelledTotal);
-            if ((travelledTotal % 2 >= 0.95) && (travelledTotal % 2 <= 1.05) || (travelledTotal == 0))
+            if (((travelledTotal % 2 >= 0.95) && (travelledTotal % 2 <= 1.05) && changeDir == false) || (travelledTotal == 0) && !patrol)
             {
                 changeDir = true;
                 direction = Random.Range(1, 9);
@@ -56,56 +81,159 @@ public class SoldierMovement : Movement
             // Move only every 5 frames (assuming 60)
             if (moveCounter == 5)
             {
-                // Player is more than 5 units away so move in a random direction
+                // Player is more than 5 units away so move in a random direction or patrol
                 if (Mathf.Pow(movementX, 2) + Mathf.Pow(movementY, 2) > 5)
                 {
-
-                    switch (direction)
+                    if (patrol)
                     {
-                        // Right
-                        case 1:
-                            MoveX = 1;
-                            MoveY = 0;
-                            break;
-                        // Up
-                        case 2:
-                            MoveX = 0;
-                            MoveY = 1;
-                            break;
-                        // Left
-                        case 3:
-                            MoveX = -1;
-                            MoveY = 0;
-                            break;
-                        // Down
-                        case 4:
-                            MoveX = 0;
-                            MoveY = -1;
-                            break;
-                        // Up right
-                        case 5:
-                            MoveX = 1;
-                            MoveY = 1;
-                            break;
-                        // Up left
-                        case 6:
-                            MoveX = -1;
-                            MoveY = 1;
-                            break;
-                        // Down left
-                        case 7:
-                            MoveX = -1;
-                            MoveY = -1;
-                            break;
-                        // Down right
-                        case 8:
-                            MoveX = 1;
-                            MoveY = -1;
-                            break;
-                        default:
-                            MoveX = 0;
-                            MoveY = 0;
-                            break;
+                        if (currentWaypoint == 1)
+                        {
+                            if((waypoints[currentWaypoint, 0] - currentPosX) > 0.1)
+                            {
+                                MoveX = 1;
+                            }
+                            else if ((waypoints[currentWaypoint, 0] - currentPosX) < -0.1)
+                            {
+                                MoveX = -1;
+                            }
+                            else
+                            {
+                                MoveX = 0;
+                            }
+
+                            if ((waypoints[currentWaypoint, 1] - currentPosY) > 0.1)
+                            {
+                                MoveY = 1;
+                            }
+                            else if ((waypoints[currentWaypoint, 1] - currentPosY) < -0.1)
+                            {
+                                MoveY = -1;
+                            }
+                            else
+                            {
+                                MoveY = 0;
+                            }
+                        }
+                        else if (currentWaypoint == 2)
+                        {
+                            if ((waypoints[currentWaypoint, 0] - currentPosX) > 0.1)
+                            {
+                                MoveX = 1;
+                            }
+                            else if ((waypoints[currentWaypoint, 0] - currentPosX) < -0.1)
+                            {
+                                MoveX = -1;
+                            }
+                            else
+                            {
+                                MoveX = 0;
+                            }
+
+                            if ((waypoints[currentWaypoint, 1] - currentPosY) > 0.1)
+                            {
+                                MoveY = 1;
+                            }
+                            else if ((waypoints[currentWaypoint, 1] - currentPosY) < -0.1)
+                            {
+                                MoveY = -1;
+                            }
+                            else
+                            {
+                                MoveY = 0;
+                            }
+                        }
+                        else
+                        {
+                            if ((waypoints[0, 0] - currentPosX) > 0.1)
+                            {
+                                MoveX = 1;
+                            }
+                            else if ((waypoints[0, 0] - currentPosX) < -0.1)
+                            {
+                                MoveX = -1;
+                            }
+                            else
+                            {
+                                MoveX = 0;
+                            }
+
+                            if ((waypoints[0, 1] - currentPosY) > 0.1)
+                            {
+                                MoveY = 1;
+                            }
+                            else if ((waypoints[0, 1] - currentPosY) < -0.1)
+                            {
+                                MoveY = -1;
+                            }
+                            else
+                            {
+                                MoveY = 0;
+                            }
+                        }
+                        // Reached destination
+                        if (((MoveX == 0) && ((MoveY == 0))))
+                        {
+                            if (currentWaypoint < 2)
+                            {
+                                currentWaypoint++;
+                            }
+                            else
+                            {
+                                currentWaypoint = 0;
+                            }
+                        }
+                    }
+
+
+                    else
+                    {
+                        switch (direction)
+                        {
+                            // Right
+                            case 1:
+                                MoveX = 1;
+                                MoveY = 0;
+                                break;
+                            // Up
+                            case 2:
+                                MoveX = 0;
+                                MoveY = 1;
+                                break;
+                            // Left
+                            case 3:
+                                MoveX = -1;
+                                MoveY = 0;
+                                break;
+                            // Down
+                            case 4:
+                                MoveX = 0;
+                                MoveY = -1;
+                                break;
+                            // Up right
+                            case 5:
+                                MoveX = 1;
+                                MoveY = 1;
+                                break;
+                            // Up left
+                            case 6:
+                                MoveX = -1;
+                                MoveY = 1;
+                                break;
+                            // Down left
+                            case 7:
+                                MoveX = -1;
+                                MoveY = -1;
+                                break;
+                            // Down right
+                            case 8:
+                                MoveX = 1;
+                                MoveY = -1;
+                                break;
+                            default:
+                                MoveX = 0;
+                                MoveY = 0;
+                                break;
+                        }
                     }
                 }
                 // Player is within 5 units so move towards them
@@ -166,6 +294,7 @@ public class SoldierMovement : Movement
         if (travelledTotal >= movement)
         {
             levelManagerScripts.changeTurn();
+            //moveSelected = false;
             travelledTotal = 0;
         }
 
@@ -173,7 +302,7 @@ public class SoldierMovement : Movement
 
     void moveEnemy(float directionX, float directionY)
     {
-        velocity = new Vector2(2 * directionX, 2 * directionY);
+        velocity = new Vector2(speedModifier * directionX, speedModifier * directionY);
         myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
     }
 
