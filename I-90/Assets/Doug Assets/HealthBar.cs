1b6d364550bb2cd53e45b1d050618f4fcd0afd44
Doug Barnes <Doug.barnes27@gmail.com>
2020-03-26 12:18:36 -0700

    small code fix

--- I-90/Assets/Doug Assets/HealthBar.cs
+++ I-90/Assets/Doug Assets/HealthBar.cs
@@ -10,12 +10,26 @@ public class HealthBar : MonoBehaviour
 {
     public Player _Player;
     public Enemy _Enemy;
+    int enemy_H, player_H, Total_H = 10;
     public Image foregroundImage;
     
+    //Fills healthbar at start
     private void Start()
     {
         foregroundImage.fillAmount = _Player.getHealth();
         foregroundImage.fillAmount = _Enemy.getHealth();
-        //_Player.damagePlayer(1);
     }
+
+    // update to change healthbar
+    private void FixedUpdate()
+    {
+        foregroundImage.fillAmount = enemy_H;
+        //foregroundImage.fillAmount = _Enemy.getHealth();
+    }
+
+    private void PercentHealth()
+    {
+        enemy_H = _Enemy.getHealth() / Total_H;
+        player_H = _Player.getHealth() / Total_H;
+    } 
 }
