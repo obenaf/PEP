fcac0ca673e2099d0edc5181bb9b9e2e100b2279
tion4994 <54964647+tion4994@users.noreply.github.com>
2020-03-30 16:43:33 -0700

    Testing

--- I-90/Assets/Editor/Nikolai_EnemyPatrolTest.cs
+++ I-90/Assets/Editor/Nikolai_EnemyPatrolTest.cs
@@ -3,22 +3,23 @@ using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
 
-/*public class Nikolai_EnemyPatrolTest : MonoBehaviour
+[TestFixture]
+public class Nikolai_EnemyPatrolTest : MonoBehaviour
 {
     private Level0Manager game;
 
-    [UnityTest]
-    public IEnumerator EnemyPatrolTest()
+    [Test]
+    public void EnemyPatrolTest()
     {
-        GameObject gameGameObject =
+        //GameObject gameGameObject =
         MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
-        game = gameGameObject.GetComponent<Level0Manager>();
+        //game = gameGameObject.GetComponent<Level0Manager>();
 
 
         Object.Destroy(game.gameObject);
     }
 
 
-}*/
+}
 
 
