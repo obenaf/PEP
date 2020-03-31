c3da37c3dc64a7dd73d15608f7e63f2bee91c5e9
Doug Barnes <Doug.barnes27@gmail.com>
2020-03-31 10:19:43 -0700

    health bar finished

---
+++ I-90/Assets/Doug Assets/EnemyHealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    public Enemy _Enemy;
+    public Image foregroundImage;
+    float Health;
+
+    // Update is called once per frame
+    void Update()
+    {
+        HealthFill();
+    }
+
+    public void HealthFill()
+    {
+        Health = _Enemy.getHealth();
+        foregroundImage.fillAmount = Health / 10f;
+        Debug.Log(Health.ToString());
+    }
+}
--- I-90/Assets/Doug Assets/PlayerHealthBar.cs
+++ I-90/Assets/Doug Assets/PlayerHealthBar.cs
@@ -5,21 +5,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-// Modified from how to make a health bar from: https://www.youtube.com/watch?v=CA2snUe7ARM
+// Modified from how to make a health bar at: https://www.youtube.com/watch?v=CA2snUe7ARM
 // Adds health bars to characters that spwan (watch later): https://www.youtube.com/watch?v=kQqqo_9FfsU
 public class PlayerHealthBar : MonoBehaviour
 {
     public Player _Player;
     public Image foregroundImage;
     float Health;
-
-    // Start is called before the first frame update
-    void Awake()
-    {
-        //Health = _Player.getHealth();
-        //foregroundImage.fillAmount = Health / 20f;
-        //Debug.Log(Health.ToString());
-    }
 
     // Update is called once per frame
     void Update()
@@ -30,8 +22,7 @@ public class PlayerHealthBar : MonoBehaviour
     public void HealthFill()
     {
         Health = _Player.getHealth();
-        foregroundImage.fillAmount = Health / 2002f;
+        foregroundImage.fillAmount = Health / 10f;
         Debug.Log(Health.ToString());
-
     }
 }
