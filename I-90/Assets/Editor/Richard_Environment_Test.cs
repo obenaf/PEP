05cbf6318eb1484605ecb600f91ec46521ce1ee7
Richard <43195127+obenaf@users.noreply.github.com>
2020-03-30 21:21:49 -0700

    Added simple environment objects test

---
+++ I-90/Assets/Editor/Richard_Environment_Test.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+//This test(s) will allow us to check that levels contain the appropriate amounts of every tagged object.
+//determines existence of extra or unnoticed objects
+
+//Can't get scene to load properly
+//Unity docs suck
+public class Richard_Test : MonoBehaviour
+{
+    private bool working = false;
+
+    [Test]
+    public void Richard_Environment_Test()
+    {
+        if(working)
+        {
+            if(Object_Check("Level0", 2, 3))
+            {
+                Assert.AreEqual(1,1);
+            }
+        }
+        //Here for now to make us feel warm and fuzzy
+        Assert.AreEqual(1,1);
+    }
+
+    bool Object_Check(string scene_name, int item_count, int enemy_count)
+    {
+        EditorSceneManager.OpenScene(scene_name);
+        if(GameObject.FindGameObjectsWithTag("item").Length == item_count)
+        {
+            if(GameObject.FindGameObjectsWithTag("enemy").Length == enemy_count)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
