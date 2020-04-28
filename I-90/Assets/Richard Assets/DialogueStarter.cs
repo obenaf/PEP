e25e9867bfecccecef2225739b94aa687ce094b9
Richard <43195127+obenaf@users.noreply.github.com>
2020-04-27 22:04:56 -0700

    Added dialogue system

---
+++ I-90/Assets/Richard Assets/DialogueStarter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueStarter : MonoBehaviour
+{
+    private GameObject DialogueCanvas;
+    void Start() {
+        DialogueCanvas = GameObject.Find("DialogueCanvas");
+        DialogueCanvas.SetActive(false);
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+            DialogueCanvas.SetActive(true);
+        }
+    }
+    void OnTriggerExit2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+            DialogueCanvas.SetActive(false);
+        }
+    }
+}
