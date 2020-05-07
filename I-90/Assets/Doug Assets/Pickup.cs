f4a159d8e58befcfefc1a9b8113ab19ea0b8d8c9
Doug Barnes <Doug.barnes27@gmail.com>
2020-05-06 22:36:17 -0700

    fixed an issue and built game

--- I-90/Assets/Doug Assets/Pickup.cs
+++ I-90/Assets/Doug Assets/Pickup.cs
@@ -1,32 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Pickup : MonoBehaviour
-{
-    private PlayerInventory inventory;
-    public GameObject itemButton;
+// public class Pickup : MonoBehaviour
+// {
+//     private PlayerInventory inventory;
+//     public GameObject itemButton;
 
-    private void Start()
-    {
-        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>();
-    }
+//     private void Start()
+//     {
+//         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>();
+//     }
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            for (int i = 0; i < inventory.slots.Length; i++)
-            {
-                if (inventory.isFull[i] == false)
-                {
+//     void OnTriggerEnter2D(Collider2D other)
+//     {
+//         if (other.CompareTag("Player"))
+//         {
+//             for (int i = 0; i < inventory.slots.Length; i++)
+//             {
+//                 if (inventory.isFull[i] == false)
+//                 {
                     // ITEM CAN BE ADDED TO INVENTORY
-                    inventory.isFull[i] = true;
-                    Instantiate(itemButton, inventory.slots[i].transform, false);
-                    Destroy(gameObject);
-                    break;
-                }
-            }
-        }
-    }
-}
+                    // inventory.isFull[i] = true;
+                    // Instantiate(itemButton, inventory.slots[i].transform, false);
+                    // Destroy(gameObject);
+                    // break;
+//                 }
+//             }
+//         }
+//     }
+// }
